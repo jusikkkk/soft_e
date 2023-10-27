b225cbb7eb42317311882423c46203ac2c588615
jusikkkk <145589493+jusikkkk@users.noreply.github.com>
2023-10-27 23:38:39 +0500

    Update 3.py

--- код/лр/3.py
+++ код/лр/3.py
@@ -3,6 +3,6 @@ import time as t
 
 star_t = t.time()
 end_t = star_t + 5
-while t.time() < end_t:
+while start_time < end_t:
     print(d.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     t.sleep(1)
