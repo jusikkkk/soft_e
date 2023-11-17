1c1929055d21cf3df1d6acb5400e946ef8429de1
jusikkkk <145589493+jusikkkk@users.noreply.github.com>
2023-11-18 01:48:37 +0500

    Update 1.py

--- код/ср/1.py
+++ код/ср/1.py
@@ -1,7 +1,14 @@
 with open('ukf.txt', encoding='utf-8') as f:
     w_count = 0
     article_line = f.read().split()
+    word_count = {}
+    for word in article_line:
+        if len(word)>2 and word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
     for words in article_line:
         w_count += 1
+    most_common_word = max(word_count, key=word_count.get)
 
-print(w_count)
+print(f"Количество слов: {w_count}\nНаиболее часто встречающееся слово: {most_common_word}")
