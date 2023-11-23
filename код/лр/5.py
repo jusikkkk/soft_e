ebc2f57fcb62b8b42ddeb23a7b8fc956bfd84b54
jusikkkk <145589493+jusikkkk@users.noreply.github.com>
2023-11-23 14:33:17 +0500

    Add files via upload

---
+++ код/лр/5.py
@@ -0,0 +1,23 @@
+class Shape:
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+rec_area = Rectangle(4,7)
+cir_area = Circle(4)
+print(rec_area.area())
+print(cir_area.area())
