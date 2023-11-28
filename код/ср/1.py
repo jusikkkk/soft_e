86056b21debcd90611f5ea19f5ab75ec75c5517b
jusikkkk <145589493+jusikkkk@users.noreply.github.com>
2023-11-28 17:02:19 +0500

    Add files via upload

--- код/ср/1.py
+++ код/ср/1.py
@@ -1,1 +1,13 @@
+class Julia:
+    __slots__ = ['name']
 
+    def __init__(self, name):
+        if name == "Юля":
+            self.name = f"Да, я {name}"
+        else:
+            self.name = f"Я не {name}, а Юля"
+
+person1 = Julia('Аня')
+person2 = Julia('Юля')
+print(person1.name)
+print(person2.name)
